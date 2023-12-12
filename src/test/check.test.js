73ba3a2ed77023525d1112671314e6f7aa405d15
Dayeon Yoon <yday1223@gmail.com>
2023-12-12 23:21:15 +0900

    mock function test with API

--- src/test/check.test.js
+++ src/test/check.test.js
@@ -12,8 +12,19 @@ describe('check', () => {
   it('should call onSuccess when predicate is true', () => {
     check(() => true, onSuccess, onFail);
 
-    expect(onSuccess.mock.calls.length).toBe(1);
-    expect(onFail.mock.calls.length).toBe(0);
-    expect(onSuccess.mock.calls[0][0]).toBe('yes');
+    // expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    // expect(onFail.mock.calls.length).toBe(0);
+    expect(onFail).toHaveBeenCalledTimes(0);
+    // expect(onSuccess.mock.calls[0][0]).toBe('yes');
+    expect(onSuccess).toHaveBeenCalledWith('yes');
+  });
+
+  it('should call onSuccess when predicate is true', () => {
+    check(() => false, onSuccess, onFail);
+
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+    expect(onFail).toHaveBeenCalledWith('no');
   });
 });
